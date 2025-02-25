09a319e4813ad46d9d8e83bb304fbbc8ea035b96
sdprg3 <113368129+sdprg3@users.noreply.github.com>
2025-02-24 22:48:22 -0600

    Delete problem3 directory

--- problem3/target/debug/Problem3.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Steven\Desktop\Newprojects\Homework2-StevenPham\problem3\target\debug\Problem3.exe: C:\Users\Steven\Desktop\Newprojects\Homework2-StevenPham\problem3\src\main.rs
--- problem3/target/debug/deps/Problem3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Steven\Desktop\Newprojects\Homework2-StevenPham\problem3\target\debug\deps\Problem3.exe: src\main.rs
-
-C:\Users\Steven\Desktop\Newprojects\Homework2-StevenPham\problem3\target\debug\deps\Problem3.d: src\main.rs
-
-src\main.rs:
